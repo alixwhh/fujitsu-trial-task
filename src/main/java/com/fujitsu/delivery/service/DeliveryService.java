f24c9336a83957f9f1aa8d115665626f752b1b80
ali <hahinn@ttu.ee>
2023-04-01 15:21:51 +0300

    Calculate extra fees based on air temperature
    If the air temperature is less than -10 degrees, then the air temperature extra fees should be 1 euro. If the air temperature is between 0 and -10 degrees, then the air temperature extra fees should be 0.5 euros.

---
+++ src/main/java/com/fujitsu/delivery/service/DeliveryService.java
@@ -0,0 +1,54 @@
+package com.fujitsu.delivery.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.fujitsu.delivery.entity.Station;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+
+@Service
+public class DeliveryService {
+
+    @Autowired
+    WeatherService weatherService;
+
+    private static final Map<String, Map<String, BigDecimal>> REGIONAL_BASE_FEES =
+            Map.of("Tallinn", Map.of("Car", BigDecimal.valueOf(4), "Scooter", BigDecimal.valueOf(3.5), "Bike", BigDecimal.valueOf(3)),
+                    "Tartu", Map.of("Car", BigDecimal.valueOf(3.5), "Scooter", BigDecimal.valueOf(3), "Bike", BigDecimal.valueOf(2.5)),
+                    "Pärnu", Map.of("Car", BigDecimal.valueOf(3), "Scooter", BigDecimal.valueOf(2.5), "Bike", BigDecimal.valueOf(2)));
+
+
+    public int getDeliveryFees(String city, String vehicleType) {
+        BigDecimal regionalBaseFees = REGIONAL_BASE_FEES.get(city).get(vehicleType);
+        BigDecimal extraFees = calculateExtraFees(city, vehicleType);
+        return 0;
+    }
+
+
+    public BigDecimal calculateExtraFees(String city, String vehicleType) {
+        BigDecimal extraFees = BigDecimal.ZERO;
+        Station currentWeatherData = getCurrentWeatherData(city);
+        if (vehicleType.equals("Scooter") || vehicleType.equals("Bike")) {
+            extraFees = extraFees.add(getAirTemperatureExtraFees(currentWeatherData));
+        }
+        return extraFees;
+    }
+
+    public BigDecimal getAirTemperatureExtraFees(Station currentWeatherData) {
+        BigDecimal extraFees = BigDecimal.ZERO;
+        double airTemperature = currentWeatherData.getAirTemperature();
+        if (airTemperature < 0) {
+            extraFees = extraFees.add(BigDecimal.valueOf(0.5));
+            if (airTemperature < -10) {
+                extraFees = extraFees.add(BigDecimal.valueOf(0.5));
+            }
+        }
+        return extraFees;
+    }
+
+    public Station getCurrentWeatherData(String city) {
+        return weatherService.getCurrentWeatherData(city);
+    }
+}
