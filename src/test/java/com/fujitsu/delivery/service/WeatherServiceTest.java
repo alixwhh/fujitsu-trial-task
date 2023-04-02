dc6357e3300e200fddf4e196b0ad25212d114b61
ali <hahinn@ttu.ee>
2023-04-02 14:00:41 +0300

    Add tests for WeatherService

---
+++ src/test/java/com/fujitsu/delivery/service/WeatherServiceTest.java
@@ -0,0 +1,37 @@
+package com.fujitsu.delivery.service;
+
+import com.fujitsu.delivery.entity.Station;
+import com.fujitsu.delivery.repository.WeatherRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+class WeatherServiceTest {
+
+    @Mock
+    private WeatherRepository weatherRepository;
+
+    @InjectMocks
+    private WeatherService weatherService;
+
+    public static final Station station = Station.builder().name("Tartu-Tõravere").WMOCode(26242)
+            .airTemperature(-2.1).windSpeed(4.7).phenomenon("Light snow shower").build();
+
+
+    @Test
+    void givenCity_whenGetCurrentWeatherData_thenShouldReturnStation() {
+        given(weatherRepository.findFirstByNameContainingIgnoreCaseOrderByIdDesc("Tartu")).willReturn(station);
+
+        var result = weatherService.getCurrentWeatherData("Tartu");
+
+        then(weatherRepository).should().findFirstByNameContainingIgnoreCaseOrderByIdDesc("Tartu");
+        assertEquals(station, result);
+    }
+}
